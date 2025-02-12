918a3ab53b9b800da80dae22e792a9093e73a314
Jacob Baker <jb00645@my.westga.edu>
2025-02-11 19:34:07 -0500

    Created SignIn Page Ui
    I created the Sign in page which also doubles as the create an account ui since both are on the same Ui page

---
+++ code/Hyre_Defyer_Project_SE2/src/edu/westga/cs3211/hyre_defyer_project/view/SignInView.java
@@ -0,0 +1,79 @@
+package edu.westga.cs3211.hyre_defyer_project.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class SignInView {
+
+    @FXML
+    private ImageView accountBioImage;
+
+    @FXML
+    private Label accountLabel;
+
+    @FXML
+    private TextField confirmPasswordCreateAccountTextFeild;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private Label dmLabel;
+
+    @FXML
+    private Label homeLabel;
+
+    @FXML
+    private Label hyreLabel;
+
+    @FXML
+    private TextField passwordCreateAccountTextFeild;
+
+    @FXML
+    private TextField passwordSignInTextFeild;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private TextField userNameCreateAccountTextFeild;
+
+    @FXML
+    private TextField userNameSignInTextFeild;
+
+    @FXML
+    void handleAccountClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleCreateAccountClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleDMClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleHomeClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleHyreClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleSignInClick(ActionEvent event) {
+
+    }
+
+}
