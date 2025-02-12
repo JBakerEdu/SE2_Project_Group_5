9c7f993c7ed8da2a0c60364eecc8448c93d6fe9f
Jacob Baker <jb00645@my.westga.edu>
2025-02-11 20:30:23 -0500

    Created AccountPage UI
    Made the account ui and am still tinkering with the way we want to do skills, thinking about combo boxes that will become labels once selected.

---
+++ code/Hyre_Defyer_Project_SE2/src/edu/westga/cs3211/hyre_defyer_project/view/AccountPageView.java
@@ -0,0 +1,63 @@
+package edu.westga.cs3211.hyre_defyer_project.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class AccountPageView {
+
+    @FXML
+    private Button ViewCatergoriesButton;
+
+    @FXML
+    private ImageView accountBioImage;
+
+    @FXML
+    private Label accountLabel;
+
+    @FXML
+    private Label dmLabel;
+
+    @FXML
+    private Label homeLabel;
+
+    @FXML
+    private Label hyreLabel;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    void handleAccountClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleDMClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleHomeClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleHyreClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleMoreCatergoriesClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSignInClick(ActionEvent event) {
+
+    }
+
+}
