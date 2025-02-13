90b0d5246d6720460dd48cc7dcd04a3845a8616a
Kate Anglin <kanglin3@my.westga.edu>
2025-02-13 14:35:33 -0500

    Added User Class
    Added Alec's User class then I add more methods to the class.
    I added set methods and did only the excpetion checking for null and empty.
    The User Class has 100% test coverage and no checkstyle errors.

--- code/Hyre_Defyer_Project_SE2/src/edu/westga/cs3211/hyre_defyer_project/model/User.java
+++ code/Hyre_Defyer_Project_SE2/src/edu/westga/cs3211/hyre_defyer_project/model/User.java
@@ -1,45 +1,131 @@
 package edu.westga.cs3211.hyre_defyer_project.model;
 
-/**
- * The User Class for our application
- * 
- * @author Kate Anglin
+/** Handles and stores information about a user​
+ *​
+ * @author Alec Neal amd Kate Anglin​
+ * @version Spring 2025​
  */
 public class User {
 	
-	private boolean loggedIn;
+	private String userName;
+	private String userBio;
+	private String password;
+	
 
-	/**
-	 * Creates a new user that is not currently logged in
+	/** 
+	 * Creates a user with a username, password, and biography.
+	 * 
+	 * @precondition userName != null && !userName.isBlank() && password != null && !password.isBlank() && userBio != null
+	 * @postcondition userName, password, and userBio are set
+	 * 
+	 * @param userName The user's name
+	 * @param password The user's password
+	 * @param userBio The user's biography
+	 * @throws IllegalArgumentException if userName or password is null or empty, or if userBio is null
 	 */
-	public User() {
-		this.setLoggedIn(false);
+	public User(String userName, String password, String userBio) {
+		this.setUserName(userName);
+		this.setPassword(password);
+		this.setBio(userBio);
+	}
+	
+	
+	/** 
+	 * Creates a user​
+	 *​
+	 * @precondition userName != null && !userName.isBlank() && password != null && !password.isBlank()
+	 * @postcondition userName and password are set, userBio is initialized as an empty string
+	 * 
+	 * @param userName The user's name
+	 * @param password The user's password​​
+	 * @throws IllegalArgumentException if userName or password is null or empty
+	 */
+	public User(String userName, String password) {
+		this(userName, password, "");
 	}
 	
 	/**
-	 * Creates a new User and shows if they are logged in
+	 * Gets the users name
+	 *
+	 * @precondition none
+	 * @postcondition none
 	 * 
-	 * @param loggedIn true if logged in; false otherwise
+	 * @return the users name
 	 */
-	public User(boolean loggedIn) {
-		this.setLoggedIn(loggedIn);
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	/**
+	 * Sets the user's name.
+	 *
+	 * @precondition userName != null && !userName.isBlank()
+	 * @postcondition this.userName is set
+	 * 
+	 * @param userName The user's name
+	 * @throws IllegalArgumentException if userName is null or empty
+	 */
+	public void setUserName(String userName) {
+		if (userName == null || userName.isBlank()) {
+			throw new IllegalArgumentException("User name cannot be empty or null.");
+		}
+		this.userName = userName;
+	}
+	
+	/**
+	 * Gets the users password
+	 *
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return the users password
+	 */
+	public String getPassword() {
+		return this.password;
 	}
 
 	/**
-	 * Gets if the user is logged in
+	 * Sets the user's password.
+	 *
+	 * @precondition password != null && !password.isBlank()
+	 * @postcondition this.password is set
 	 * 
-	 * @return true if logged in; false otherwise
+	 * @param password The user's password
+	 * @throws IllegalArgumentException if password is null or empty
 	 */
-	public boolean isLoggedIn() {
-		return this.loggedIn;
+	public void setPassword(String password) {
+		if (password == null || password.isBlank()) {
+			throw new IllegalArgumentException("Password cannot be empty or null.");
+		}
+		this.password = password;
 	}
-
+	
 	/**
-	 * Sets the logged in value 
+	 * Gets the users biography
+	 *
+	 * @precondition none
+	 * @postcondition none
 	 * 
-	 * @param loggedIn
+	 * @return the users name
 	 */
-	public void setLoggedIn(boolean loggedIn) {
-		this.loggedIn = loggedIn;
+	public String getBio() {
+		return this.userBio;
 	}
+	
+	/**
+	 * Sets the user's biography.
+	 *
+	 * @precondition bio != null
+	 * @postcondition this.userBio is set
+	 * 
+	 * @param bio The user's biography
+	 * @throws IllegalArgumentException if bio is null
+	 */
+	public void setBio(String bio) {
+		if (bio == null) {
+			throw new IllegalArgumentException("Bio cannot be null.");
+		}
+		this.userBio = bio;
+	}
+	
 }
