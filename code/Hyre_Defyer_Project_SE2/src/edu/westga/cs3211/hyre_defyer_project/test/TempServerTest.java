bbe32c1059e77d3ff528d5d2acc2cd311064879d
AlecNeal <alecxwneal@gmail.com>
2025-03-13 12:49:22 -0400

    Fixed static tests

--- code/Hyre_Defyer_Project_SE2/src/edu/westga/cs3211/hyre_defyer_project/test/TempServerTest.java
+++ code/Hyre_Defyer_Project_SE2/src/edu/westga/cs3211/hyre_defyer_project/test/TempServerTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import edu.westga.cs3211.hyre_defyer_project.model.DirectMessageHandler;
@@ -22,16 +23,17 @@ public class TempServerTest {
         assertEquals("user1", user.getUserName());
         assertEquals("password1", user.getPassword());
         assertEquals("", user.getBio());
+        
     }
 	
 	@Test
 	public void testSendMessage() {
-        ServerActor.createAccount("user1", "password1");
-        ServerActor.createAccount("user2", "password2");
-        ServerActor.createAccount("user3", "password3");
-        User user1 = ServerActor.login("user1", "password1");
-        User user2 = ServerActor.login("user2", "password2");
-        User user3 = ServerActor.login("user3", "password3");
+        ServerActor.createAccount("paul", "password1");
+        ServerActor.createAccount("todd", "password2");
+        ServerActor.createAccount("karen", "password3");
+        User user1 = ServerActor.login("paul", "password1");
+        User user2 = ServerActor.login("todd", "password2");
+        User user3 = ServerActor.login("karen", "password3");
         DirectMessageHandler handler = new DirectMessageHandler(user1, user2);
         DirectMessageHandler handler1 = new DirectMessageHandler(user1, user3);
         Message message = new Message("Hello", user1, user2);
@@ -44,17 +46,17 @@ public class TempServerTest {
 	
 	@Test
 	public void testDuplicateAccount() {
-		assertTrue(ServerActor.createAccount("user1", "password1"));
+		assertTrue(ServerActor.createAccount("kate", "password1"));
 		
-        assertFalse(ServerActor.createAccount("user1", "password2"));
+        assertFalse(ServerActor.createAccount("kate", "password2"));
 
 	}
 	
 	@Test
 	public void testInvalidLogin() {
-		assertTrue(ServerActor.createAccount("user1", "password1"));
+		assertTrue(ServerActor.createAccount("jacob", "password1"));
 		
-        assertNull(ServerActor.login("user1", "notpassword"));
+        assertNull(ServerActor.login("jacob", "notpassword"));
 
 	}
 	
