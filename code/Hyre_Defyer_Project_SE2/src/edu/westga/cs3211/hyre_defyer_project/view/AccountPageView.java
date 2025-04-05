3748c988f533872f651079f18ace507a7836f2ad
Miles Debro <kd00245@my.westga.edu>
2025-04-05 18:13:22 -0400

    Implemented Front-end
    1) Users can now delete their account via a button they can use when editing their account.

--- code/Hyre_Defyer_Project_SE2/src/edu/westga/cs3211/hyre_defyer_project/view/AccountPageView.java
+++ code/Hyre_Defyer_Project_SE2/src/edu/westga/cs3211/hyre_defyer_project/view/AccountPageView.java
@@ -45,6 +45,9 @@ public class AccountPageView {
 
     @FXML
     private Button cancelButton;
+    
+    @FXML
+    private Button deleteAccountButton;
 
     @FXML
     private Label categoryLabel;
@@ -160,6 +163,13 @@ public class AccountPageView {
     }
     
     @FXML
+    void handleDeleteAccountButtonClick(ActionEvent event) {
+    	ServerInterface.deleteUser(SignInViewModel.getCurrentUser().getUserName());
+    	SignInViewModel.signOut();
+    	GUIHelper.switchView(anchorPane, Views.HOMEPAGE);
+    }
+    
+    @FXML
     void handleSaveClick(ActionEvent event) {
         User selectedUser = AccountPageViewModel.getUserSelectedToView();
         if (selectedUser == null) {
@@ -223,6 +233,7 @@ public class AccountPageView {
     private void toggleEditMode(boolean isEditing) {
     	this.saveButton.setVisible(isEditing);
     	this.cancelButton.setVisible(isEditing);
+    	this.deleteAccountButton.setVisible(isEditing);
     	this.editButton.setVisible(!isEditing);
     	this.descriptionTextBox.setEditable(isEditing);
     	if (this.isFreelancer) {
